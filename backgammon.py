52644764258fa35cd8ccae5eeda569f8cc7f4887
Fabian Schierok <fabian.schierok@udo.edu>
2020-08-25 12:27:38 +0200

    Copy Index.html and set a route to it

--- backgammon.py
+++ backgammon.py
@@ -3,4 +3,8 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/game", methods=["GET", "POST"])
+def game():
     return render_template("game.html")
