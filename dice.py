2ff21904376d88de98370331ec815ed5f742b197
Simon-Neuhaus <simon.neuhaus@tu-dortmund.de>
2020-08-26 14:46:32 +0200

    change dice.py that RollTwoDice()
    RollTwoDice() returns both dice instant of the sum.
    remove the function RollTwoDiceWhichGreater().

--- dice.py
+++ dice.py
@@ -14,19 +14,5 @@ def RollOneDice(): # Würfeln mit einem Würfel
 def RollTwoDice(): # Würfeln mit zwei Würfeln
     w1 = RollOneDice()
     w2 = RollOneDice()
-    w = w1 + w2
-    return [w, w1==w2] # Würfelergebnis und Bool ob Pasch
+    return [w1, w2]
 
-def RollTwoDiceWhichGreater():
-    w1 = RollOneDice()
-    w2 = RollOneDice()
-    w = w1 + w2
-    if w1<w2: # 2. Würfel zeigt höhere Zahl an
-        return [w, 2]
-    elif w1>w2: # 1. Würfel zeigt höhere Zahl an
-        return [w, 1]
-    elif w1==w2: # Pasch
-        return [w, -1] # die -1 ist willkürlich gewählt, sodass diese nicht mit einem der beiden Würfel verwechselt werden kann.
-    else: 
-        print("ERROR")
-        return [w, 404]
